488b6acd4be4f859a8513f7d020c2cb6fc95a9c1
Jeremiah Lewis <--get>
2024-03-13 15:34:56 +0100

    archive commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
+using DistributedReinforcementLearning
 using Test
-using ReinforcementLearning
+using Distributed
+using ReinforcementLearningBase
+using ReinforcementLearningCore
+using ReinforcementLearningZoo
+using ReinforcementLearningEnvironments
+using Flux
 
-@testset "ReinforcementLearning" begin
+@testset "DistributedReinforcementLearning.jl" begin
+
+include("actor.jl")
+include("core.jl")
+
 end
